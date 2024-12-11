19e656b3684931432a71ea56e2ee9b82182fd145
mage <1303219055@qq.com>
2024-12-11 19:46:40 +0800

    激活函数和初始化部分结束

---
+++ wide&depp/activation.py
@@ -0,0 +1,32 @@
+
+from turtle import forward
+import numpy as np
+
+class Sigmod:
+    def __init__(self):
+        self._last_forward_result = None
+
+    def forward(self,X):
+        self._last_forward_result = 1.0/(1+np.exp(-X))
+        return self._last_forward_result
+    
+    def backward(self,prev_grads):
+        assert self._last_forward_result.shape == prev_grads.shape
+
+        return prev_grads*self._last_forward_result*(1-self._last_forward_result) #后面的是sigmod的导数
+    
+
+class Relu:
+    def __init__(self):
+        self._last_input = None
+
+    def forward(self,X):
+        self._last_input = X
+        return np.maximum(0, X)
+    
+    def backward(self,prev_grads):
+        assert prev_grads.shape == self._last_input.shape
+        local_grads = np.zeros_like(self._last_input)
+        local_grads[self._last_input > 0] = 1.0
+
+        return prev_grads * local_grads
--- wide&depp/initialization.py
+++ wide&depp/initialization.py
@@ -15,3 +15,45 @@ class TruncatedNormal: #z-score
     
     def __call__(self, shape):
         return self._rand.rvs(size=shape)
+
+
+class Zero:   
+    def __call__(self,shape):
+        return np.zeros(shape)
+
+
+
+class GlorotUniform:
+    def __call__(self,shape):
+        fan_in,fan_out = shape
+        scale = np.sqrt(6 / (fan_in + fan_out))
+        return np.random.uniform(-scale,scale,shape)
+    
+
+class GlorotNormal:
+    def __call__(self, shape):
+        fan_in, fan_out = shape
+        stdev = np.sqrt(2 / (fan_out + fan_in))
+        return np.random.normal(loc=0, scale=stdev, size=shape)
+
+
+_Global_Initializers = {} # initializers which can be shared
+
+    
+def get_global_init(name):
+    if name in _Global_Initializers:
+        return _Global_Initializers[name]
+
+    if name == "zero":
+        initializer = Zero()
+    elif name == "glorot_uniform":
+        initializer = GlorotUniform()
+    elif name == "glorot_normal":
+        initializer = GlorotNormal()
+    else:
+        raise ValueError('unknown initializer={}'.format(name))
+
+    _Global_Initializers[name] = initializer
+    return initializer
+    
+
